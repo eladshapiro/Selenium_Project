a0a20325785d97a790756b7d698d1de4d789e1b9
EladShapiro <eladshapiro@edu.aac.ac.il>
2021-07-26 21:26:15 +0300

    little changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,15 +14,25 @@ public class Main
         System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.aac.ac.il/");
-        driver.findElement(By.className("info-btn")).findElement(By.tagName("a")).click();
+        WebElement personalInfo=driver.findElement(By.className("info-btn")).findElement(By.tagName("a"));
+        System.out.println(personalInfo.getText());
+        personalInfo.click();
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
         WebElement userName=driver.findElement(By.id("Ecom_User_ID"));
-        if (userName!=null) {
+        if (userName!=null)
+        {
             userName.sendKeys("dvirkazayof");
         }
-        WebElement password=driver.findElement(By.id("Ecom_Password"));
+        WebElement password= driver.findElement(By.id("Ecom_Password"));
         password.sendKeys("ofrit1204");
         driver.findElement(By.id("wp-submit")).click();
         driver.get(driver.getCurrentUrl());
-
     }
 }
