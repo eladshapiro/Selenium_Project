9c22e402f963800d7e26d345b60abfd6afa0523d
dvirkaza <dvirkazayof@edu.aac.ac.il>
2021-07-26 19:18:05 +0300

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
 public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.aac.ac.il/");
+        driver.findElement(By.className("info-btn")).findElement(By.tagName("a")).click();
+        WebElement userName=driver.findElement(By.id("Ecom_User_ID"));
+        if (userName!=null) {
+            userName.sendKeys("dvirkazayof");
+        }
+        WebElement password=driver.findElement(By.id("Ecom_Password"));
+        password.sendKeys("ofrit1204");
+        driver.findElement(By.id("wp-submit")).click();
+        driver.get(driver.getCurrentUrl());
+
+
+    }
 }
