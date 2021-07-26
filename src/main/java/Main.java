5ed021d3007e7a6b439b11f14f819dc75ed3488f
EladShapiro <eladshapiro@edu.aac.ac.il>
2021-07-26 22:06:34 +0300

    new methid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
 
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException {
+      try {
+          enter();
+      }
+      catch (IOException e)
+      {
+
+      }
+
+
+    }
+
+
+    public static void enter() throws IOException
     {
         //dvir: "webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe"
         //Elad : "webdriver.chrome.driver","C:\\Users\\User\\chromedriver_win32\\chromedriver.exe"
@@ -18,14 +32,8 @@ public class Main
         System.out.println(personalInfo.getText());
         personalInfo.click();
         driver.manage().window().maximize();
-        try
-        {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+        driver.get(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
         WebElement userName=driver.findElement(By.id("Ecom_User_ID"));
         if (userName!=null)
         {
