7b22cf182b46df266103b753c81995fd4a8245b9
dvirkaza <dvirkazayof@edu.aac.ac.il>
2021-07-27 08:19:13 +0300

    הצלחתי להיכנס למידע אישי

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class Main
@@ -25,15 +27,27 @@ public class Main
     {
         //dvir: "webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe"
         //Elad : "webdriver.chrome.driver","C:\\Users\\User\\chromedriver_win32\\chromedriver.exe"
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.aac.ac.il/");
         WebElement personalInfo=driver.findElement(By.className("info-btn")).findElement(By.tagName("a"));
         System.out.println(personalInfo.getText());
         personalInfo.click();
         driver.manage().window().maximize();
-        driver.get(driver.getCurrentUrl());
-        System.out.println(driver.getTitle());
+        String MainWindow=driver.getWindowHandle();
+
+        // To handle all new opened window.
+        Set<String> s1=driver.getWindowHandles();
+        Iterator<String> i1=s1.iterator();
+       while(i1.hasNext()) {
+            String ChildWindow = i1.next();
+
+            if (!MainWindow.equalsIgnoreCase(ChildWindow)) {
+
+                // Switching to Child window
+                driver.switchTo().window(ChildWindow);
+            }
+        }
         WebElement userName=driver.findElement(By.id("Ecom_User_ID"));
         if (userName!=null)
         {
@@ -42,6 +56,5 @@ public class Main
         WebElement password= driver.findElement(By.id("Ecom_Password"));
         password.sendKeys("ofrit1204");
         driver.findElement(By.id("wp-submit")).click();
-        driver.get(driver.getCurrentUrl());
     }
 }
